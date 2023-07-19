b016a9237e8406770c6c7a4bd93f35077288c80e
Noam Green <Noamgr11@gmail.com>
2023-07-19 03:40:38 +0300

    portfolio done- with mediaqury sample-img

---
+++ src/services/portfolio.service.jsx
@@ -0,0 +1,55 @@
+import IMG1 from '../assets/img/portfolio1.jpg'
+import IMG2 from '../assets/img/portfolio2.jpg'
+import IMG3 from '../assets/img/portfolio3.jpg'
+import IMG4 from '../assets/img/portfolio4.jpg'
+import IMG5 from '../assets/img/portfolio5.png'
+import IMG6 from '../assets/img/portfolio6.jpg'
+
+
+export const ProtfolioProj = [
+  {
+    id: 1,
+    img: IMG1,
+    title: 'Musify',
+    github: 'https://github.com/TediM98/Musify',
+    demo: 'https://musify-fullstack.onrender.com/'
+  },
+  {
+    id: 2,
+    img: IMG2,
+    title: 'Musify',
+    github: 'https://github.com/TediM98/Musify',
+    demo: 'https://musify-fullstack.onrender.com/'
+  },
+  {
+    id: 3,
+    img: IMG3,
+    title: 'Musify',
+    github: 'https://github.com/TediM98/Musify',
+    demo: 'https://musify-fullstack.onrender.com/'
+  },
+  {
+    id: 4,
+    img: IMG4,
+    title: 'Musify',
+    github: 'https://github.com/TediM98/Musify',
+    demo: 'https://musify-fullstack.onrender.com/'
+  },
+  {
+    id: 5,
+    img: IMG5,
+    title: 'Musify',
+    github: 'https://github.com/TediM98/Musify',
+    demo: 'https://musify-fullstack.onrender.com/'
+  },
+  {
+    id: 6,
+    img: IMG6,
+    title: 'Musify',
+    github: 'https://github.com/TediM98/Musify',
+    demo: 'https://musify-fullstack.onrender.com/'
+  }
+
+
+
+]
