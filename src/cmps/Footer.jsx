5df1d6d0a5798cb526dc14eb08d4a923bc4273fb
Noam Green <Noamgr11@gmail.com>
2023-07-19 05:11:36 +0300

    finish comp footer with media queries

--- src/cmps/Footer.jsx
+++ src/cmps/Footer.jsx
@@ -1,7 +1,35 @@
+import { FaFacebookF } from 'react-icons/fa'
+import { FiInstagram } from 'react-icons/fi'
+
 export function Footer() {
 
 
   return (
-    <h1>footer</h1>
+    <footer>
+      <a href="#" className="footer-logo">Noamg</a>
+
+      <ul className="parmalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer-socials">
+        <a href="https://www.facebook.com/noam.green.420/">
+          <FaFacebookF />
+        </a>
+        <a href="https://www.instagram.com/">
+          <FiInstagram />
+        </a>
+      </div>
+
+      <div className="footer-copyright">
+        <small>&copy; Noam Green. All rights reserved</small>
+      </div>
+
+    </footer>
   )
 }
