d0136b62a0f1b5afb12683b65fbfd9af5b5dc62c
Noam Green <Noamgr11@gmail.com>
2023-07-19 11:43:20 +0300

    github pages bug fixing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom'
 import './assets/styles/main.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>
+  </React.StrictMode>
 );
 
 reportWebVitals();
