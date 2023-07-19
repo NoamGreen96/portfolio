f120d0b470ddb24e2f237a2c5ab7b72aa8bbbba8
Noam Green <Noamgr11@gmail.com>
2023-07-19 11:24:32 +0300

    check github pages upload bug

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'
 import './assets/styles/main.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.Fragment>
+  <Router>
     <App />
-  </React.Fragment>
+  </Router>
 );
 
 reportWebVitals();
