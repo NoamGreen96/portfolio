818f877de2f6bb3b47c11754b6475e5e7f791634
unknown <noam@moovingon.com>
2025-01-04 23:18:34 +0200

    change info

--- src/services/about.service.jsx
+++ src/services/about.service.jsx
@@ -1,10 +1,10 @@
 export function AboutP() {
-  const aboutContent = `Hello there! 😊
+  const aboutContent = `Hello there!
     I'm Noam Green, 
-    A 27-year-old passionate Frontend | Full Stack Web Developer from Kiryat Ata - Israel. 🚀 
+    A 28-year-old passionate Full Stack Web Developer from Kiryat Motzkin - Israel. 🚀 
     My expertise spans across a range of cutting-edge technologies, including React.js, Redux, Node.js, and MongoDB. 🔧 
     With a focus on targeted solutions, I excel at managing states with Redux and crafting intuitive user interfaces using the latest technologies. 💻 
-    Looking forward to connecting with you! 🌟`;
+    `;
 
   const formattedContent = aboutContent.replace(/\n\s+/g, "<br/>");
   return <p dangerouslySetInnerHTML={{ __html: formattedContent }} />;
