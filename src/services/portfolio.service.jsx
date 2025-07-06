b458817f624dfa525baef03b323fc949ece26485
unknown <noamgr11@gmail.com>
2025-07-06 18:00:57 +0300

    new changes

--- src/services/portfolio.service.jsx
+++ src/services/portfolio.service.jsx
@@ -1,53 +1,43 @@
 import IMG1 from '../assets/img/portfolio1.jpg'
-import IMG2 from '../assets/img/portfolio2.jpg'
-import IMG3 from '../assets/img/portfolio3.jpg'
+import IMG2 from '../assets/img/ZCRUM.jpg'
+import IMG3 from '../assets/img/portfolio2.jpg'
 import IMG4 from '../assets/img/portfolio4.jpg'
-import IMG5 from '../assets/img/portfolio5.png'
-import IMG6 from '../assets/img/portfolio6.jpg'
-
+import IMG5 from '../assets/img/portfolio3.jpg'
 
 export const ProtfolioProj = [
   {
     id: 1,
-    img: IMG1,
+    img: IMG4,
     title: 'Musify - React',
     github: 'https://github.com/TediM98/Musify',
-    demo: 'https://musify-fullstack.onrender.com/'
+    demo: 'https://musify-fullstack.onrender.com/',
   },
   {
     id: 2,
     img: IMG2,
-    title: 'Mr-bitcoin - Vue',
-    github: 'https://github.com/NoamGreen96/Mr-bitcoin',
-    demo: 'https://noamgreen96.github.io/Mr-bitcoin/#/'
+    title: 'ZCRUM',
+    github: 'https://github.com/NoamGreen96/jira-clone',
+    demo: 'https://jira-clone-henna-psi.vercel.app/',
   },
   {
     id: 3,
     img: IMG3,
-    title: 'AppSus',
-    github: 'https://github.com/NoamGreen96/Appsus',
-    demo: 'https://noamgreen96.github.io/Appsus/'
+    title: 'Mr-bitcoin - Vue',
+    github: 'https://github.com/NoamGreen96/Mr-bitcoin',
+    demo: 'https://noamgreen96.github.io/Mr-bitcoin/#/',
   },
   {
     id: 4,
-    img: IMG4,
+    img: IMG1,
     title: 'Minesweeper',
     github: 'https://noamgreen96.github.io/Minesweeper-proj/',
-    demo: 'https://github.com/NoamGreen96/Minesweeper-proj'
+    demo: 'https://github.com/NoamGreen96/Minesweeper-proj',
   },
-  // {
-  //   id: 5,
-  //   img: IMG5,
-  //   title: '',
-  //   github: '',
-  //   demo: ''
-  // },
-  // {
-  //   id: 6,
-  //   img: IMG6,
-  //   title: '',
-  //   github: '',
-  //   demo: ''
-  // }
-
-]
+  {
+    id: 5,
+    img: IMG5,
+    title: 'AppSus',
+    github: 'https://github.com/NoamGreen96/Appsus',
+    demo: 'https://noamgreen96.github.io/Appsus/',
+  },
+];
